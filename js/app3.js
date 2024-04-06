d4963801d721397a767c954b7d8684c284b5fcc8
danilosheen <c.danilo.f.silva@gmail.com>
2024-04-06 12:00:24 -0300

    feat: app 3

--- js/app3.js
+++ js/app3.js
@@ -1,20 +1,17 @@
-import { Cliente } from "./classes/cliente";
-import { ContaCorrente } from "./classes/contaCorrente";
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const cliente_1 = require("./classes/cliente");
+const contaCorrente_1 = require("./classes/contaCorrente");
 //Crie um cliente que possua uma ContaCorrente
-const cc1 = new ContaCorrente(1, 0);
-const cliente1 = new Cliente(999999, "Danilo", "88888888", true, cc1);
-
+const cc1 = new contaCorrente_1.ContaCorrente(1, 0);
+const cliente1 = new cliente_1.Cliente(999999, "Danilo", "88888888", true, cc1);
 //Efetue três depósitos de 100 nessa ContaCorrente
 cc1.depositar(100, new Date());
 cc1.depositar(100, new Date());
 cc1.depositar(100, new Date());
-
 //Efetue um saque de 50 reais nessa ContaCorrente
 cc1.sacar(50, new Date());
-
 //Imprima o valor do saldo dessa ContaCorrente
 console.log(cc1.verLimite());
-
 console.log("-----------------------------------");
 console.log(cc1);
